62c612e2385e355f9c77d56ea0c53443eb173246
tadashi-aikawa <syou.maman@gmail.com>
2017-05-09 23:34:53 +0900

    :hammer: Update ngh option

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ ng build --prod --base-href https://tadashi-aikawa.github.io/jumeaux-viewer/
 git config --global user.email "syou.maman@gmail.com"
 git config --global user.name "tadashi-aikawa"
 
-ngh --no-silent --repo=https://${GHP_TOKEN}@github.com/tadashi-aikawa/jumeaux-viewer.git
+ngh --repo=https://${GHP_TOKEN}@github.com/tadashi-aikawa/jumeaux-viewer.git
