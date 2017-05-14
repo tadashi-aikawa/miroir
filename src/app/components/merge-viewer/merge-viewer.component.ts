e2fe71fc28594781cb60e8c49c34cf27c0040199
tadashi-aikawa <syou.maman@gmail.com>
2017-05-15 01:40:54 +0900

    :fire: Remove key binding when editor is focused

--- src/app/components/merge-viewer/merge-viewer.component.ts
+++ src/app/components/merge-viewer/merge-viewer.component.ts
@@ -20,17 +20,6 @@ export class MergeViewerComponent implements AfterViewInit, OnDestroy {
 
     @ViewChild('view') view: ElementRef;
 
-    @Output() onKeyD = new EventEmitter<void>();
-    @Output() onKeyI = new EventEmitter<boolean>();
-    @Output() onKeyJ = new EventEmitter<void>();
-    @Output() onKeyK = new EventEmitter<boolean>();
-    @Output() onKeyL = new EventEmitter<void>();
-    @Output() onKeyP = new EventEmitter<void>();
-    @Output() onKeyQ = new EventEmitter<void>();
-    @Output() onKeyW = new EventEmitter<void>();
-    @Output() onKeySlash = new EventEmitter<void>();
-    @Output() onKeyQuestion = new EventEmitter<void>();
-
     private _updateLayout: Function;
 
     constructor() {
@@ -48,7 +37,6 @@ export class MergeViewerComponent implements AfterViewInit, OnDestroy {
                     renderSideBySide: this.config.sideBySide,
                     scrollBeyondLastLine: false
                 });
-                this.bindKeys(this.diffEditor);
                 w.addEventListener('resize', this._updateLayout);
 
                 this.diffNavigator = monaco.editor.createDiffNavigator(this.diffEditor);
@@ -91,16 +79,4 @@ export class MergeViewerComponent implements AfterViewInit, OnDestroy {
         this.diffEditor.layout();
     }
 
-    private bindKeys(diffEditor) {
-        diffEditor.addCommand(monaco.KeyCode.KEY_D, () => this.onKeyD.emit());
-        diffEditor.addCommand(monaco.KeyCode.KEY_I, () => this.onKeyI.emit());
-        diffEditor.addCommand(monaco.KeyCode.KEY_J, () => this.onKeyJ.emit());
-        diffEditor.addCommand(monaco.KeyCode.KEY_K, () => this.onKeyK.emit());
-        diffEditor.addCommand(monaco.KeyCode.KEY_L, () => this.onKeyL.emit());
-        diffEditor.addCommand(monaco.KeyCode.KEY_P, () => this.onKeyP.emit());
-        diffEditor.addCommand(monaco.KeyCode.KEY_Q, () => this.onKeyQ.emit());
-        diffEditor.addCommand(monaco.KeyCode.KEY_W, () => this.onKeyW.emit());
-        diffEditor.addCommand(monaco.KeyCode.US_SLASH, () => this.onKeySlash.emit());
-        diffEditor.addCommand(monaco.KeyMod.Shift | monaco.KeyCode.US_SLASH, () => this.onKeyI.emit());
-    }
 }
