150b61ad6ca0cee6b567f74499ee9e3cec32d550
tadashi-aikawa <syou.maman@gmail.com>
2017-06-05 21:30:06 +0900

    Revert ":hammer: Remove unused function"
    This reverts commit b9e7cbe4eef8017f0d8d883890ce6018bba364b0.

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -47,4 +47,8 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
     updateView() {
         this.editor.layout();
     }
+
+    getValue(): string {
+        return this.editor.getValue();
+    }
 }
