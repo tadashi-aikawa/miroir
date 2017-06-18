6ae874bd7aa1c0dcfea023c8e1f5a6ef51fb6de1
tadashi-aikawa <syou.maman@gmail.com>
2017-06-19 01:04:35 +0900

    :skull: Fix bug that can't edit check point yaml

--- src/app/components/detail-dialog/detail-dialog.component.ts
+++ src/app/components/detail-dialog/detail-dialog.component.ts
@@ -218,7 +218,7 @@ cases:
           - root<'items'><[0-9]><'hogehoge-removed'>
           `,
             contentType: 'yaml',
-            readOnly: true,
+            readOnly: false,
             theme: 'vs'
         };
 
