5e1a4d379f201942b385ed3e134e4fc157e41982
tadashi-aikawa <syou.maman@gmail.com>
2017-07-14 08:29:30 +0900

    :arrow_upper_right: Make animation of loading to be smooth

--- src/app/components/guard/logo-loading.component.ts
+++ src/app/components/guard/logo-loading.component.ts
@@ -1,7 +1,23 @@
 import {Component} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'app-logo-loading',
+    animations: [
+        trigger(
+            'feed',
+            [
+                transition(':enter', [
+                    style({opacity: 0}),
+                    animate('300ms', style({opacity: 1}))
+                ]),
+                transition(':leave', [
+                    style({opacity: 1}),
+                    animate('300ms', style({opacity: 0}))
+                ])
+            ]
+        )
+    ],
     template: `
         <style>
             @-webkit-keyframes blink {
@@ -19,7 +35,7 @@ import {Component} from '@angular/core';
                 -webkit-animation-iteration-count:infinite;
                 -webkit-animation-timing-function:ease-in-out;
                 -webkit-animation-direction: alternate;
-                -webkit-animation-delay: 0s;
+                -webkit-animation-delay: 0.3s;
             }
 
             .frame {
@@ -30,14 +46,13 @@ import {Component} from '@angular/core';
                 align-items: center;
                 z-index: 100;
                 position: absolute;
-                color: white;
-                background-color: rgba(0, 0, 0, 0.75);
+                background-color: rgba(255, 255, 255, 0.85);
             }
         </style>
         <div class="frame">
-            <div class="center-horizontal blink">
-                <img src="assets/jumeaux-white.png" style="width: 50%; height: 50%;" />
-                <p style="font-size: 125%">Now Loading...</p>
+            <div class="center-horizontal blink" [@feed]>
+                <img src="assets/jumeaux.png" style="width: 50%; height: 50%;" />
+                <p style="font-size: 150%"><b>Now Loading...</b></p>
                 <md-progress-bar mode="indeterminate"></md-progress-bar>
             </div>
         </div>
