f131077c60916753c747e23340c5657c7ad93289
tadashi-aikawa <syou.maman@gmail.com>
2017-07-17 14:13:21 +0900

    :arrow_upper_right: Improve logos

--- src/app/components/guard/logo-loading.component.ts
+++ src/app/components/guard/logo-loading.component.ts
@@ -3,58 +3,26 @@ import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'app-logo-loading',
-    animations: [
-        trigger(
-            'feed',
-            [
-                transition(':enter', [
-                    style({opacity: 0}),
-                    animate('300ms', style({opacity: 1}))
-                ]),
-                transition(':leave', [
-                    style({opacity: 1}),
-                    animate('300ms', style({opacity: 0}))
-                ])
-            ]
-        )
-    ],
     template: `
         <style>
-            @-webkit-keyframes blink {
-                from {
-                    opacity: 1;
-                }
-                to {
-                    opacity: 0.1;
-                }
-            }
-
-            .blink {
-                -webkit-animation-name: blink;
-                -webkit-animation-duration: 0.65s;
-                -webkit-animation-iteration-count:infinite;
-                -webkit-animation-timing-function:ease-in-out;
-                -webkit-animation-direction: alternate;
-                -webkit-animation-delay: 0.3s;
-            }
-
             .frame {
                 width: 100%;
                 height: 100%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
                 z-index: 100;
-                position: absolute;
+                position: fixed;
                 background-color: rgba(255, 255, 255, 0.85);
             }
+            .loading-image {
+                width: auto;
+                height: 75vh;
+                max-height: 768px;
+                top: 10vh;
+                z-index: 200;
+                position: fixed;
+            }
         </style>
-        <div class="frame">
-            <div class="center-horizontal blink" [@feed]>
-                <img src="assets/jumeaux.png" style="width: 50%; height: 50%;" />
-                <p style="font-size: 150%"><b>Now Loading...</b></p>
-                <md-progress-bar mode="indeterminate"></md-progress-bar>
-            </div>
+        <div class="frame center-horizontal">
+            <img src="assets/jumeaux-loading.gif" class="loading-image"/>
         </div>
     `
 })
