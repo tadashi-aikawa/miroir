03fe71bbce1ec27962903a759f6294e54a7291e5
tadashi-aikawa <syou.maman@gmail.com>
2018-01-02 18:38:20 +0900

    :arrow_upper_right: Validate settings
    (cherry picked from commit a767d1a)

--- src/app/components/root/root.component.ts
+++ src/app/components/root/root.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {AwsService} from '../../services/aws-service';
-import {ToasterConfig, ToasterService} from "angular2-toaster";
+import {ToasterConfig} from "angular2-toaster";
 import {SettingsService} from "../../services/settings-service";
+
 const {version} = require('../../../../package.json');
 
 @Component({
@@ -12,9 +13,19 @@ const {version} = require('../../../../package.json');
 export class RootComponent {
     version: string = version;
     region: string = this.awsService.region;
+
     table: string = this.awsService.table;
+    connectingTable: boolean;
+    tableError: string;
+
     bucket: string = this.awsService.bucket;
+    connectingBucket: boolean;
+    bucketError: string;
+
     prefix: string = this.awsService.prefix;
+    connectingPrefix: boolean;
+    prefixError: string;
+
     useLocalStack: boolean = this.awsService.useLocalStack;
     localStackEndpoint: string = this.awsService.localStackEndpoint;
     alwaysIntelligentAnalytics: boolean = this.settingsService.alwaysIntelligentAnalytics;
@@ -27,6 +38,51 @@ export class RootComponent {
 
     constructor(private awsService: AwsService, private settingsService: SettingsService) {
         // DO NOTHING
+    }
+
+    pingTable() {
+        this.connectingTable = true;
+        this.awsService.pingTable(this.table)
+            .then(_ => {
+                this.tableError = undefined;
+                this.connectingTable = false;
+            })
+            .catch((err: string) => {
+                this.tableError = err;
+                this.connectingTable = false;
+            });
+    }
+
+    pingBucket() {
+        this.connectingBucket = true;
+        this.awsService.pingBucket(this.bucket)
+            .then(_ => {
+                this.bucketError = undefined;
+                this.connectingBucket = false;
+            })
+            .catch((err: string) => {
+                this.bucketError = err;
+                this.connectingBucket = false;
+            });
+    }
+
+    pingPrefix() {
+        this.connectingPrefix = true;
+        this.awsService.pingBucketWithPrefix(this.bucket, this.prefix)
+            .then(_ => {
+                this.prefixError = undefined;
+                this.connectingPrefix = false;
+            })
+            .catch((err: string) => {
+                this.prefixError = err;
+                this.connectingPrefix = false;
+            });
+    }
+
+    pingAll() {
+        this.pingTable();
+        this.pingBucket();
+        this.pingPrefix();
     }
 
     update() {
