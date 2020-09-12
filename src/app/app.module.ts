8162e77cdd28579eef332b5ce7b85a014e08f41c
tadashi-aikawa <syou.maman@gmail.com>
2020-09-12 16:17:12 +0900

    http -> common

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {
     MatAutocompleteModule,
     MatButtonModule,
@@ -112,7 +112,7 @@ const appRoutes: Routes = [
         RouterModule.forRoot(appRoutes),
         FormsModule,
         ReactiveFormsModule,
-        HttpModule,
+        HttpClientModule,
         NgSelectModule, FormsModule,
         CommonModule,
         Ng2HighchartsModule,
